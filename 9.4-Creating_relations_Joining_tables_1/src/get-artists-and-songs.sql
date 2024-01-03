3ecf03d274ade0e0e22b8d1a1481913eaf9646c3
Akil Franklin <akilf@indeed.com>
2024-01-03 16:37:26 -0600

    joins assessment

---
+++ 9.4-Creating_relations_Joining_tables_1/src/get-artists-and-songs.sql
@@ -0,0 +1,4 @@
+select * 
+from artists a
+join songs s
+on a.artist_id = s.artist;
---
+++ 9.4-Creating_relations_Joining_tables_1/src/get-artists-by-song-name.sql
@@ -0,0 +1,5 @@
+select a.artist_name, s.song_name, s.album_name
+from artists a
+inner join songs s
+on a.artist_id = s.artist
+where s.song_name like 'The%';
