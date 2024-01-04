68f9bd7e56e3f60f97b80656a5e336eb23a31525
Akil Franklin <akilf@indeed.com>
2024-01-04 16:22:53 -0600

    knex async await

---
+++ 10.3-knex/src/products/products.controller.js
@@ -0,0 +1,28 @@
+const productsService = require("./products.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
+function read(req, res) {
+  const { product: data } = res.locals;
+  res.json({ data });
+}
+
+async function list(req, res, next) {
+  const data = await productsService.list();
+  res.json({ data });
+}
+
+
+async function productExists(req, res, next) {
+  const product = await productsService.read(req.params.productId);
+  if (product) {
+    res.locals.product = product;
+    return next();
+  }
+  next({ status: 404, message: `Product cannot be found.` });
+}
+
+
+module.exports = {
+  read: [productExists, asyncErrorBoundary(read)],
+  list: [asyncErrorBoundary(list)],
+};
